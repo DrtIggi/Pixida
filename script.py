75427e4851f21b17f8dbdf5cd388fd1975120666
Ignat <i3378047@gmail.com>
2021-11-26 20:58:07 +0100

    First testik

--- script.py
+++ script.py
@@ -1,1 +1,18 @@
-import telebot
+import telebot, sqlite3
+
+gpt_key = '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'
+
+token = '2117240577:AAF1McmhAsPNTfrMh3BDeRojzDe_K50r4XY'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Hi!")
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == "__main__":
+    bot.polling()
